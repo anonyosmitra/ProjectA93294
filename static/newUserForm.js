ff5720e9b0b1f74295714577c4106aac2ebe6bb6
andro <anonyos.mitra@gmail.com>
2021-06-29 23:42:57 +0530

    A

--- static/newUserForm.js
+++ static/newUserForm.js
@@ -2,9 +2,10 @@ function makeForm(){
  ReactDOM.render(<Form />, document.getElementById("formBox"));}
 function Form(){
     return(
-		<div>
+		<form>
 		<Name /><br></br>
-		</div>
+		<Dob /><br></br>
+		</form>
 	)}
 class Name extends React.Component {
         onChange(){
@@ -20,6 +21,32 @@ class Name extends React.Component {
           <input onBlur={this.onChange} name="name"></input>
           <span id="nameWarning" style={{color:"red",cursor: "default"}} hidden>
             <abbr title="Invalid Name">&#9888;</abbr>
+          </span></div>)
+        }
+      }
+class Dob extends React.Component {
+        onChange(){
+            var dob=event.target.value;
+            var hide=true
+            var b=dbo.split("-")
+            for(var i in b)
+                b[i]=parseInt(b[i])
+            now=new Date()
+            if(now.getFullYear<b[0]+18)
+                hide=false
+            else if(now.getFullYear==b[0]+18)
+                if (now.getMonth+1<b[1])
+                    hide=false
+                else if (now.getMonth+1==b[1])
+                     if (now.getDate<b[2])
+                        hide=false
+            document.getElementById("dobWarning").hidden=hide;
+        }
+        render() {
+          return (<div><b>Date of Birth:</b>
+          <input onBlur={this.onChange} name="dob" type="date"></input>
+          <span id="dobWarning" style={{color:"red",cursor: "default"}} hidden>
+            <abbr title="User must be about 18!">&#9888;</abbr>
           </span></div>)
         }
       }
