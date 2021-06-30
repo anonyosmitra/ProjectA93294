5ee2fa3802684250b63fc11359757d4d17b74c12
andro <anonyos.mitra@gmail.com>
2021-06-30 07:57:02 +0530

    A

--- mailer.py
+++ mailer.py
@@ -14,4 +14,4 @@ def send(receiverId,name):
 	context = ssl.create_default_context()
 	with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
 		server.login(sender_email, password)
-		server.senadmail(sender_email, receiver_email, message)
+		server.sendmail(sender_email, receiver_email, message)
