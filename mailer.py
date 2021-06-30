7c2d19e7c9829a46d33137294ea70632be916414
andro <anonyos.mitra@gmail.com>
2021-06-30 08:29:34 +0530

    A

--- mailer.py
+++ mailer.py
@@ -1,11 +1,10 @@
 import smtplib, ssl
-from email.mime.multipart import MIMEMultipart
-from email.mime.text import MIMEText
+
 port = 465  # For SSL
 smtp_server = "smtp.gmail.com"
 sender_email = "x3800177@gmail.com"
 password = ("x380@0000")
-def send(receiverId,name,url):
+def send(receiverId,name):
 	receiver_email = receiverId
 	message = """\
 	Mailing System
@@ -13,36 +12,7 @@ def send(receiverId,name,url):
 
 Hi %s! Your new account has been registered."""%(name)
 
-	msg = MIMEMultipart('alternative')
-	msg['Subject'] = "User Registered"
-	msg['From'] = sender_email
-	msg['To'] = receiverId
-	# Plain-text version of content
-	plain_text = """\
-	Hi %s! Your new account has been registered.
-	For More Info Visit %s"""%(name,url)
-	# html version of content
-	html_content = """\
-	    <html>
-	    <head></head>
-	    <body>
-	        <p>Hi %s,</p>
-	        <p>Your new account has been registered.</p>
-	        <p>For More Info Visit
-	            <a href="%s">
-	                %s
-	            </a>
-	        </p>
-	    </body>
-	</html>
-	"""%(name,url,url)
-
-	text_part = MIMEText(plain_text, 'plain')
-	html_part = MIMEText(html_content, 'html')
-
-	msg.attach(text_part)
-	msg.attach(html_part)
 	context = ssl.create_default_context()
 	with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
 		server.login(sender_email, password)
-		server.sendmail(to_addrs=receiverId,msg=msg,from_addr=sender_email)
+		server.sendmail(sender_email, receiver_email, message)
