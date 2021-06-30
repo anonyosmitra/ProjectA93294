beecbb88b41ddaffd340170dfa469e48fe03d8d1
andro <anonyos.mitra@gmail.com>
2021-06-30 08:47:31 +0530

    A

--- static/userList.js
+++ static/userList.js
@@ -1,13 +1,13 @@
 function makeList(props){
     ReactDOM.render(<list info={props}/>, document.getElementById("formBox"));}
  function List(props){
-    const users= props.info.map((user) =>
+    const users= props.info.map(user) =>
         (<tr key={user["rowid"]}>
         <td>{user["name"]}</td>
         <td>{user["dob"]}</td>
         <td>{user["email"]}</td>
         <td>{user["phone"]}</td>
-        </tr>),
+        </tr>)
         return(<table>
      <tr><td>Name</td><td>Date of birth</td><td>Email</td><td>Phone</td></tr>
        {users}
